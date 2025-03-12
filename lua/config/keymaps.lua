a71bdb9e89c45aaa83e5aa7fb592be9975c22f7f
eric-dishman-ctct <eric_dishman@trimble.com>
2025-03-12 09:42:00 -0500

    Updates to keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,11 +4,17 @@
 
 local map = vim.keymap.set
 
+-- open Lazy
+map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+
 -- quit
 map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
 
 -- save and quit
 map('n', '<leader>wwq', '<cmd>wq<cr>', { desc = 'Save and Quit' })
+
+-- save current buffer
+map('n', '<leader>bs', '<cmd>w<cr>', { desc = 'Save current buffer' })
 
 -- better indenting
 map('v', '<', '<gv')
@@ -17,3 +23,11 @@ map('v', '>', '>gv')
 -- commenting
 map('n', 'gco', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add Comment Below' })
 map('n', 'gcO', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add Comment Above' })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
