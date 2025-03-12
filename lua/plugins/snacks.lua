ca769a117b3e172ad5ad91ca91e4e6f915d6251b
eric-dishman-ctct <eric_dishman@trimble.com>
2025-03-12 17:43:14 -0500

    Cleaned up snacks.lua
    Removed commented configuration

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,23 +1,6 @@
-return 
+return
 {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  -- opts = {
-  --   scope = { enabled = true },
-  --   statuscolumn = { enabled = false }, -- we set this in options.lua
-  --   -- toggle = { map = LazyVim.safe_keymap_set },
-  --   words = { enabled = false },
-  -- },
-  -- stylua: ignore
-  -- keys = {
-  --   { "<leader>n", function()
-  --     if Snacks.config.picker and Snacks.config.picker.enabled then
-  --       Snacks.picker.notifications()
-  --     else
-  --       Snacks.notifier.show_history()
-  --     end
-  --   end, desc = "Notification History" },
-  --   { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-  -- },
 }
