0626eba605a22cc0705d222736dce047a634ebbd
eric-dishman-ctct <eric_dishman@trimble.com>
2025-03-12 17:42:40 -0500

    Added mini.statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,9 @@
 return {
   'echasnovski/mini.nvim',
-  version = '*'
+    enabled = true,
+  version = '*',
+  config = function()
+        local statusline = require('mini.statusline')
+        statusline.setup({ use_icons = true })
+  end
 }
