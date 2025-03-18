addd85b1031a85d45809f533dbe40ae205c1df98
eric-dishman-ctct <eric_dishman@trimble.com>
2025-03-18 12:55:52 -0500

    Before deleting PowerShell registry

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,13 +15,36 @@ return
     },
   },
   config = function()
-    require('lspconfig').lua_ls.setup {}
+    local lsp = require('lspconfig')
+    lsp.matlab_ls.setup({
+      cmd = { 'matlab-language-server', '--stdio' },
+      filetypes = { 'matlab' },
+      root_dir = function(fname)
+        return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+      end,
+      single_file_support = false,
+      settings = {
+        MATLAB = {
+          indexWorkSpace = true,
+          installPath = 'C:\\MATLAB\\R2021b',
+          matlabConnectionTiming = 'onStart',
+          telemetry = false,
+        },
+      },
+    })
+    lsp.lua_ls.setup {}
+    print("Running powershell_es setup()")
+    lsp.powershell_es.setup {
+      -- cmd = { 'pwsh', '-NoLogo', '-NoProfile', '-Command', vim.fn.stdpath "data" .. '/servers/PSES/PowerShellEditorServices/Start-EditorServices.ps1', 'Stdio' }
+      bundle_path = vim.fn.stdpath "data" .. '/servers/PSES',
+    }
 
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
-
+        print("Vader rules")
+        if not client then return end
+        print("Join the dark side")
         if client:supports_method('textDocument/formatting') then
           -- Format the current buffer on save
           vim.api.nvim_create_autocmd('BufWritePre', {
